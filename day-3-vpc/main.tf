498ba718e33dc16b488dd65ca2c778b0b15b83b2
Dnyaneshwar Dhas <dnyaneshwardhas2004@gmail.com>
2026-07-25 00:15:21 +0530

    day-3 vpc subnet routtable instance

--- day-3-vpc/main.tf
+++ day-3-vpc/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.my_vpc.id 
     cidr_block = var.public_subnet_cidr
-    availability_zone = var.public_az
+    availability_zone = var.pulic_az
     map_public_ip_on_launch = true 
     tags = {
         Name = "public-subnet"
--- day-3-vpc/output.tf
+++ day-3-vpc/output.tf
@@ -9,7 +9,3 @@ output "sg_id" {
 output "instance_id" {
     value = aws_instance.public_instance.id 
 }
-
-output "public_subnet_id" {
-    value = aws_subnet.public_subnet.id 
-}
