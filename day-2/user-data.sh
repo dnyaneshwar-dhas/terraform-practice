b66c3ac419c5ae9ef70d35e30eba9fd2b78f771b
Dnyaneshwar Dhas <dnyaneshwardhas2004@gmail.com>
2026-07-24 19:34:38 +0530

    create a instance and sg using http

--- day-2/user-data.sh
+++ day-2/user-data.sh
@@ -3,4 +3,4 @@ sudo dnf update
 sudo dnf install httpd -y
 sudo systemctl enable httpd.service
 sudo systemctl start httpd.service
-sudo echo "<h1> Welcome to terraform </h1>" > /var/www/html/index.html
+sudo echo "<h1> welcome to Dnyanu terraform practice world </h1>" > /var/www/html/index.html 
