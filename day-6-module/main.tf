c529048c81ce452819118150145c3f1864e03cd8
Dnyaneshwar Dhas <dnyaneshwardhas2004@gmail.com>
2026-07-28 20:53:42 +0530

    start module and create the vpc

---
+++ day-6-module/main.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+    source = "./module/vpc"
+    vpc_cidr = "10.0.0.0/16"
+    public_subnet_1_cidr = "10.0.0.0/24"
+    public_subnet_2_cidr = "10.0.1.0/24"
+    public_subnet_1_az = "us-east-2a"
+    public_subnet_2_az = "us-east-2b"
+    sg_name = "my-security-group"
+
+}
